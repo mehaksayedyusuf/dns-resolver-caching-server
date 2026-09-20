58fc37701ed4446f43683c1afeb930743e1eab24
Mehak Sayed Yusuf <mehaksayed35@gmail.com>
2026-09-20 17:05:49 +0530

    Initialize Go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dns-resolver-caching-server
+
+go 1.27.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("DNS Resolver and Caching Server")
+	fmt.Println("Go environment is working.")
+}
